b21bab73cf973d9675e191d169fcdf4d383f6baf
diger <admin@freenet59.ru>
2014-12-17 15:19:17 +0500

    fix patch for devel/gmake

--- devel/gmake/patch-arscan.c
+++ devel/gmake/patch-arscan.c
@@ -2,12 +2,12 @@ $NetBSD$
 
 --- arscan.c.orig	2010-07-13 01:20:32.000000000 +0000
 +++ arscan.c
-@@ -253,7 +253,7 @@ ar_scan (const char *archive, ar_member_
+@@ -325,7 +325,7 @@
  #endif
  
  #ifndef WINDOWS32
 -# if !defined (__ANDROID__) && !defined (__BEOS__)
-+# if # if !defined (__ANDROID__) && !defined (__BEOS__) && !defined (__HAIKU__)
++# if !defined (__ANDROID__) && !defined (__BEOS__) && !defined (__HAIKU__)
  #  include <ar.h>
  # else
-    /* BeOS 5 doesn't have <ar.h> but has archives in the same format
+    /* These platforms don't have <ar.h> but have archives in the same format
