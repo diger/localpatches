9ccbfa730eee1b71f71358597b193952b9677d68
diger <digervv@gmail.com>
2024-05-02 16:49:24 +0500

    update perl5 patch

--- lang/perl5/patch-hints_haiku.sh
+++ lang/perl5/patch-hints_haiku.sh
@@ -1,47 +1,11 @@
---- hints/haiku.sh.orig	2020-06-14 23:01:25.776208384 +0000
-+++ hints/haiku.sh	2020-11-20 10:42:37.247988224 +0000
-@@ -1,44 +1,2 @@
- # Haiku hints file
- # $Id$
--
--case "$prefix" in
--'') prefix="/boot/common" ;;
--*) ;; # pass the user supplied value through
--esac
--
--libpth='/boot/home/config/lib /boot/common/lib /system/lib'
--usrinc='/boot/develop/headers/posix'
--locinc='/boot/home/config/include /boot/common/include /boot/develop/headers'
--
--libc='/system/lib/libroot.so'
--libs='-lnetwork'
--
--# Use Haiku's malloc() by default.
--case "$usemymalloc" in
--'') usemymalloc='n' ;;
--esac
--
--# Haiku generally supports hard links, but the default file system (BFS)
--# doesn't. So better avoid using hard links.
--d_link='undef'
--dont_use_nlink='define'
--
--# The array syserrlst[] is useless for the most part.
--# Large negative numbers really kind of suck in arrays.
--d_syserrlst='undef'
--
--# Haiku uses gcc.
--cc="gcc"
--ld='gcc'
--
--# The runtime loader library path variable is LIBRARY_PATH.
--case "$ldlibpthname" in
--'') ldlibpthname=LIBRARY_PATH ;;
--esac
--
--# as of alpha 4.1 (at the latest) some symbols are versioned,
--# confusing the nm lookup
--case "$usenm" in
--'') usenm='undef' ;;
--esac
--
+--- hints/haiku.sh.orig	2024-05-02 15:02:26.265289728 +0500
++++ hints/haiku.sh	2024-05-02 15:02:39.139460608 +0500
+@@ -31,7 +31,7 @@
+ cc="gcc"
+ ld='gcc'
+ 
+-ccflags="$ccflags -fno-stack-protector"
++ccflags="$ccflags -fno-stack-protector -DNO_POSIX_2008_LOCALE"
+ 
+ # The runtime loader library path variable is LIBRARY_PATH.
+ case "$ldlibpthname" in
