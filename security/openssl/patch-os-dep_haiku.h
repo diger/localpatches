5548cab9175586fcdc67307da0a469aa482e9458
diger <digervv@gmail.com>
2024-05-08 09:17:44 +0500

    Remove openssl patches

--- security/openssl/patch-os-dep_haiku.h
+++
@@ -1,9 +0,0 @@
-$NetBSD$
-
---- os-dep/haiku.h.orig	2020-09-22 12:55:07.788004864 +0000
-+++ os-dep/haiku.h
-@@ -1,2 +1,4 @@
- #include <sys/select.h>
- #include <sys/time.h>
-+
-+#define OPENSSL_NO_SECURE_MEMORY
